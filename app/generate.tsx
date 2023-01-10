ca163b97166d60fc26595dde6b1feb926f5cf456
Omer Mohideen <omermohideen2004@gmail.com>
2023-01-11 01:45:28 +0530

    fetch completed activity and display

--- app/generate.tsx
+++ app/generate.tsx
@@ -1,22 +1,23 @@
 'use client';
-import React, { use, useState } from 'react'
+import React, { use } from 'react'
+import { useRouter } from "next/navigation";
 
 async function getActivities() {
   return await (await fetch("https://www.boredapi.com/api/activity/")).json()
 }
 
-
 function Generate() {
- const updateViewport = (event: { preventDefault: () => void; }) => {
-    event.preventDefault();
-    console.log(1)
+  const router = useRouter();
+  const updateViewport = (event: { preventDefault: () => void; }) => {
+   event.preventDefault();
+   router.refresh()
   };
   const a = use(getActivities())
   return (
      <div className='p-16 flex flex-col justify-center items-center gap-5'>
         <div className='font-bold text-black'>{a.activity}</div>
       <form onSubmit={updateViewport} className='flex space-x-2 space-y-2 flex-wrap justify-center items-baseline'>
-        <button type="submit"  className='rounded-lg px-4 py-2 bg-blue-500 text-blue-100 hover:bg-blue-600 duration-300'>Generate</button>
+        <button type="submit" className='rounded-xl px-4 py-2 bg-blue-500 text-md text-blue-100 hover:bg-blue-600 duration-300'>Generate</button>
       </form>
     </div>
   )
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,39 @@
 import React from 'react'
-import Generate from './generate'
+import dynamic from 'next/dynamic';
+const Generate = dynamic(
+    async () => (await import('./generate')),
+    { ssr: false }
+);
 
-function page() {
+async function getCompleted() {
+    const res = await fetch('http://127.0.0.1:8090/api/collections/activities/records?page=1&perPage=30')
+    const data = await res.json()
+    return data?.items as any[]
+}
+
+function Activity({ a }: any) {
+  const { activity } = a || {};
   return (
-    <Generate/>
+      <h5 className='text-md bg-green-100 py-2 px-2 rounded-lg'>{activity}</h5>
+  );
+}
+
+async function page() {
+  const datas = await getCompleted()
+  return (
+    <div className='flex flex-col justify-center items-center'>
+      <Generate />
+      <div>
+        {/* <h1 className='text-2xl font-bold'>In Progress</h1> */}
+
+        <h1 className='text-2xl font-bold'>Completed</h1>
+        <div className='pt-6 flex flex-col justify-center items-start gap-3'>
+            {datas?.map((data) => {
+                return <Activity key={data.id} a={data} />;
+            })}
+        </div>
+      </div>
+    </div>
   )
 }
 
